65f86d07b7e18ecc3ece04c070160449a121179c
ravihansa <ravihansad@gmail.com>
2025-01-29 15:19:46 +0530

    Create http exception filter

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,25 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+    catch(exception: any, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+
+        let status = HttpStatus.INTERNAL_SERVER_ERROR;
+        let message = 'Internal Server Error';
+
+        if (exception instanceof HttpException) {
+            status = exception.getStatus();
+            const errorResponse = exception.getResponse();
+            message = (errorResponse as any).message || message;
+        }
+
+        response.status(status).json({
+            status: false,
+            message,
+            data: null,
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
